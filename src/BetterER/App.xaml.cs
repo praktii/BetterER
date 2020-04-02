fdc820f3bc13ff63b98deb085d90fe0747732d2e
praktii <hendrik@roskamp.me>
2020-04-02 23:33:44 +0200

    added splashscreen and build file

---
+++ src/BetterER/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BetterER
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var splashScreen = new Dialog.SplashScreen();
+            this.MainWindow = splashScreen;
+            splashScreen.Show();
+
+            Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(10000);
+                this.Dispatcher.Invoke(() =>
+                {
+                    var mainWindow = new MainWindow();
+                    this.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    splashScreen.Close();
+                });
+            });
+        }
+    }
+}
