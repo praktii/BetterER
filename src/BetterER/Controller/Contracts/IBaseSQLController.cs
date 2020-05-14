dd24c1b2baa9632c29c50166cf5aa8ffd5a9a7fd
hrs <hendrik@roskamp.me>
2020-05-14 13:16:06 +0200

    #7: added basic sql/mysql handlers and fucntions

---
+++ src/BetterER/Controller/Contracts/IBaseSQLController.cs
@@ -0,0 +1,10 @@
+﻿using BetterER.Models;
+
+namespace BetterER.Controller.Contracts
+{
+    public interface IBaseSQLController
+    {
+        string CreateTableScript(BasicEntity basicEntity);
+        string CreateTableScriptWithFullAttributes(BasicEntity basicEntity);
+    }
+}
---
+++ src/BetterER/Models/BasicAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace BetterER.Models
+{
+    public class BasicAttribute
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool NotNull { get; set; }
+        public string Default { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
