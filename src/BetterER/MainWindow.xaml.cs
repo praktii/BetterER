723a9eec71bbb56b9b9f3111e61d7bf25edebc82
Hendrik Roskamp <hendrik@roskamp.me>
2021-03-14 17:22:02 +0100

    #28 added fluentribbon

--- src/BetterER/MainWindow.xaml.cs
+++ src/BetterER/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿namespace BetterER
+﻿using Fluent;
+
+namespace BetterER
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
